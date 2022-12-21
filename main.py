0ead4fd61be2e4b7fbd891cb26c49c9808cd2ed3
Sabbir H. Mukul <107918169+seomukul@users.noreply.github.com>
2022-12-21 19:18:15 +0600

    new

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-import requests
 import streamlit as st
-from bs4 import BeautifulSoup
-url = "https://en.wikipedia.org/wiki/Steve_Jobs"
-response = requests.get(url)
-html = response.text
-soup = BeautifulSoup(html, "html.parser")
-text = soup.get_text()
-doc = nlp(text)
-entities = [ent.text for ent in doc.ents]
-
-# Count the frequency of each entity
-entity_counts = {}
-for entity in entities:
-    if entity in entity_counts:
-        entity_counts[entity] += 1
-    else:
-        entity_counts[entity] = 1
-
-# Extract the entities and their counts into separate lists
-labels = list(entity_counts.keys())
-values = list(entity_counts.values())
-
-# Create the bar chart
-st.bar_chart(values, labels)
+import wikipedia
+import nltk
+url = st.text_input("https://en.wikipedia.org/wiki/Steve_Jobs:")
+page = wikipedia.page(url=url)
+content = page.content
+tokens = nltk.word_tokenize(content)
+fdist = nltk.FreqDist(tokens)
+common_words = fdist.most_common(20)
+st.write("Most common words:", common_words)
